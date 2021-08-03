25a0e0552e7a43b53ae0b15b05e80f130183eed7
Juan Manuel Cañabate <juanmanuel@patagonian.it>
2021-08-03 19:49:49 -0300

    add color switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 import classnames from 'classnames';
+import themes from './themes/Themes.module.css';
+
+const { DarkA, DarkB } = themes;
 
 function App() {
+  const [theme, setTheme] = useState(themes.DarkA);
+
   return (
-    <div className={classnames(styles.App)}>
+    <div className={classnames(styles.App, theme)}>
+      <button
+        onClick={() => {
+          if (theme === DarkA) {
+            setTheme(DarkB);
+          } else {
+            setTheme(DarkA);
+          }
+        }}
+      >
+        {theme}
+      </button>
       <div>Hello</div>
     </div>
   );
