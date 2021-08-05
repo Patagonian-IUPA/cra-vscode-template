a54b0d39898f2099c3875530e4ed3892a503869a
Juan Manuel Cañabate <juanmanuel@patagonian.it>
2021-08-05 20:10:55 -0300

    support primary theme

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import cl from 'classnames';
-
+import themes from './themes.module.css';
 import styles from './Button.module.css';
 
 export const Button = ({ type, className, ...rest }) => {
   const claseTipo =
     type === 'regular' || type === undefined ? styles.regular : styles.primary;
 
-  return <button className={cl(styles.base, claseTipo, className)} {...rest} />;
+  return (
+    <button
+      className={cl(styles.base, themes.ThemeA, claseTipo, className)}
+      {...rest}
+    />
+  );
 };
 
 export default Button;
